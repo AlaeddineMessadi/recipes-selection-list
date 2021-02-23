a4dcc0305783ae540954d624d7bd56ab6221cfb9
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2021-02-23 21:08:30 +0100

    test(recipeslist): should render 21 recipes

--- src/modules/recipes/RecipesList.test.js
+++ src/modules/recipes/RecipesList.test.js
@@ -6,11 +6,22 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 describe('RecipesList: *****', () => {
   test('RecipesList should render without crashing', async () => {
     render(<RecipesList />);
-    // container.debug();
   });
 
   test('RecipesList should fetch data', async () => {
     const container = render(<RecipesList />);
     await waitFor(() => expect(container.getByText('WEEK OF OCTOBER 12TH')).toBeInTheDocument());
+  });
+
+  test('RecipesList should render list of 21 recipes', async () => {
+    const { container, getByText, debug } = render(<RecipesList />);
+
+    // wait untill fetched and render data
+    await waitFor(() => expect(getByText('WEEK OF OCTOBER 12TH')).toBeInTheDocument());
+
+    const listRecipes = await container.querySelectorAll(
+      '[data-name="col-sm-12 col-md-6 col-xl-4"]'
+    );
+    expect(listRecipes.length).toBe(21);
   });
 });
