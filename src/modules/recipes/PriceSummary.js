dc78f920aac442562742ebc4d37c38e14387fc7d
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2021-02-23 20:32:06 +0100

    feat(pricesummary): priceSummary styling

--- src/modules/recipes/PriceSummary.js
+++ src/modules/recipes/PriceSummary.js
@@ -2,29 +2,49 @@ import React from 'react';
 import Box from '../../components/Box';
 import { Col, Row } from '../../components/Grid';
 import Flex from '../../components/Flex';
+import Text from '../../components/Text';
 
 // Create PriceSummary user interface
 const PriceSummary = ({ summary, totalPrice }) => (
-  <Box width={['290px', '450px']}>
+  <Box width={['290px', '450px']} margin="sm">
     <Box borderBottomWidth="sm" borderBottomColor="border" borderBottomStyle="solid">
       {summary.map((recipe, index) => (
         <Row key={index}>
           <Col sm={10}>
             <Flex alignItems="center" justifyContent="flex-start">
-              <h4>{recipe.title}</h4>
+              <Text fontSize="md" lineHeight="md" fontWeight="regular" marginBottom="xs">
+                {recipe.title}
+              </Text>
             </Flex>
           </Col>
           <Col sm={2}>
             <Flex alignItems="center" justifyContent="flex-end">
-              <Box textAlign="right" mr="xs">
-                <h3>{recipe.price}</h3>
-              </Box>
+              <Text fontSize="md" lineHeight="md" fontWeight="regular" marginBottom="xs">
+                {recipe.price}
+              </Text>
             </Flex>
           </Col>
         </Row>
       ))}
     </Box>
-    <p>REPLACE ME!</p>
+    <Box>
+      <Row>
+        <Col sm={10}>
+          <Flex alignItems="center" justifyContent="flex-start">
+            <Text fontSize="md" lineHeight="md" fontWeight="bold" marginTop="xs">
+              Total
+            </Text>
+          </Flex>
+        </Col>
+        <Col sm={2}>
+          <Flex alignItems="center" justifyContent="flex-end">
+            <Text fontSize="md" lineHeight="md" fontWeight="bold" marginTop="xs">
+              {totalPrice}
+            </Text>
+          </Flex>
+        </Col>
+      </Row>
+    </Box>
   </Box>
 );
 
